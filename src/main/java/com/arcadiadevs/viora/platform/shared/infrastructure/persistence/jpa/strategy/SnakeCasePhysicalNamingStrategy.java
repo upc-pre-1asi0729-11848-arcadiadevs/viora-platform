3f54ad90c7ffdcc91edddc30755b259981d57b99
peruvianMiau <miauperuvian@gmail.com>
2026-05-26 12:43:38 -0500

    feat(shared): convert catalog name, the shema name and the table name to snake case.

---
+++ src/main/java/com/arcadiadevs/viora/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -0,0 +1,27 @@
+package com.arcadiadevs.viora.platform.shared.infrastructure.persistence.jpa.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
+
+public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy{
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(this.toPlural(identifier));
+    }
+
+
+}
