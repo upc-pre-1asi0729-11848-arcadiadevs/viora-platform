def3076ef5367de908da8e1802c51e3fde869a11
victor <daroncameloftdev@gmail.com>
2026-05-26 11:54:39 -0500

    chore: initial commit.

---
+++ src/main/java/com/arcadiadevs/viora/platform/VioraPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.arcadiadevs.viora.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioraPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VioraPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcadiadevs/viora/platform/VioraPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadiadevs.viora.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioraPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
