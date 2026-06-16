d73fc1fb716d834f7fac3b556859b770fbaaebd4
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2026-06-16 09:17:32 -0500

    feat(surveillance): add aggregate values for surveillance.

---
+++ src/main/java/com/arcadiadevs/viora/platform/surveillance/domain/model/valueobjects/AlertSource.java
@@ -0,0 +1,10 @@
+package com.arcadiadevs.viora.platform.surveillance.domain.model.valueobjects;
+
+public enum AlertSource {
+    CLIMATE,
+    MANUAL_REPORT,
+    COMMUNITY,
+    SATELLITE,
+    IOT,
+    SYSTEM
+}
