857ea751a1f6dcbe43efce9ed56e16189757b2e1
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2026-06-15 19:02:19 -0500

    feat(surveillance): add query service and query for all symptoms.

---
+++ src/main/java/com/arcadiadevs/viora/platform/surveillance/application/queryservices/SymptomQueryService.java
@@ -0,0 +1,22 @@
+package com.arcadiadevs.viora.platform.surveillance.application.queryservices;
+
+import com.arcadiadevs.viora.platform.surveillance.domain.model.aggregates.SymptomDictionaryItem;
+import com.arcadiadevs.viora.platform.surveillance.domain.model.queries.GetAllSymptomsQuery;
+import com.arcadiadevs.viora.platform.surveillance.domain.repositories.SymptomDictionaryItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SymptomQueryService {
+
+    private final SymptomDictionaryItemRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<SymptomDictionaryItem> handle(GetAllSymptomsQuery query) {
+        return repository.findAll();
+    }
+}
