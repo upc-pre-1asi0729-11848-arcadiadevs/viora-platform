b5d1bcc2191a289a5ccbc267650021a49af2a389
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-06-29 14:21:17 -0500

    feat(intervention): mockup for intervention request metrics query service.

---
+++ src/main/java/com/arcadiadevs/viora/platform/intervention/application/queryservices/InterventionRequestMetricsQueryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.arcadiadevs.viora.platform.intervention.application.queryservices;
+
+import com.arcadiadevs.viora.platform.intervention.domain.model.queries.GetGrowerRequestMetricsQuery;
+import com.arcadiadevs.viora.platform.intervention.domain.model.queries.GetSpecialistRequestMetricsQuery;
+import com.arcadiadevs.viora.platform.intervention.domain.services.InterventionRequestMetricsQueryService;
+import com.arcadiadevs.viora.platform.intervention.interfaces.rest.resources.ActiveAlertResource;
+import com.arcadiadevs.viora.platform.intervention.interfaces.rest.resources.GrowerRequestMetricsResource;
+import com.arcadiadevs.viora.platform.intervention.interfaces.rest.resources.SpecialistRequestMetricsResource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of the {@link InterventionRequestMetricsQueryService}.
+ */
+@Service
+public class InterventionRequestMetricsQueryServiceImpl implements InterventionRequestMetricsQueryService {
+
+    @Override
+    public GrowerRequestMetricsResource handle(GetGrowerRequestMetricsQuery query) {
+        // TODO: Implement actual logic using repositories and ACLs
+        return new GrowerRequestMetricsResource(
+                new ActiveAlertResource(1L, "Placeholder Alert", "Tacna", "4.2 ha"),
+                0,
+                "PENDING"
+        );
+    }
+
+    @Override
+    public SpecialistRequestMetricsResource handle(GetSpecialistRequestMetricsQuery query) {
+        // TODO: Implement actual logic using repositories
+        return new SpecialistRequestMetricsResource(0, 0, 0);
+    }
+}
