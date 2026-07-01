c7effd252870cbc7eac49918c0c73eff1134979a
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-06-30 20:03:55 -0500

    feat(intervention): add grace period, hire again, impact level, impact report, service evation and service report value objects.

---
+++ src/main/java/com/arcadiadevs/viora/platform/intervention/domain/model/valueobjects/ImpactLevel.java
@@ -0,0 +1,8 @@
+package com.arcadiadevs.viora.platform.intervention.domain.model.valueobjects;
+
+public enum ImpactLevel {
+    POSITIVE,
+    PARTIAL,
+    NEGATIVE,
+    INCONCLUSIVE
+}
---
+++ src/main/java/com/arcadiadevs/viora/platform/intervention/domain/model/valueobjects/ServiceResult.java
@@ -0,0 +1,7 @@
+package com.arcadiadevs.viora.platform.intervention.domain.model.valueobjects;
+
+public enum ServiceResult {
+    RESOLVED,
+    PARTIALLY_RESOLVED,
+    NOT_RESOLVED
+}
