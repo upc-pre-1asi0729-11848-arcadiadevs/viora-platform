42c95fb98b9bfae5d98445059e140c4eeaf67b8d
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-07-01 10:33:27 -0500

    feat(auth): add role, password and roles value objects, with user aggregate.

---
+++ src/main/java/com/arcadiadevs/viora/platform/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,12 @@
+package com.arcadiadevs.viora.platform.iam.domain.model.valueobjects;
+
+/**
+ * Roles
+ * <p>
+ *     This enum represents the roles in the system.
+ * </p>
+ */
+public enum Roles {
+    ROLE_GROWER,
+    ROLE_SPECIALIST
+}
