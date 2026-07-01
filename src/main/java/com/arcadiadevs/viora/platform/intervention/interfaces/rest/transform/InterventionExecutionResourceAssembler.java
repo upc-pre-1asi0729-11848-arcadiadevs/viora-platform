086c0068bb33d2a2b4fc98b7057feed38fd614b3
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-06-30 19:06:54 -0500

    feat(intervention): add resources for intervention execution.

---
+++ src/main/java/com/arcadiadevs/viora/platform/intervention/interfaces/rest/transform/InterventionExecutionResourceAssembler.java
@@ -0,0 +1,32 @@
+package com.arcadiadevs.viora.platform.intervention.interfaces.rest.transform;
+
+import com.arcadiadevs.viora.platform.intervention.domain.model.aggregates.InterventionExecution;
+import com.arcadiadevs.viora.platform.intervention.domain.model.commands.CertifyApplicationCommand;
+import com.arcadiadevs.viora.platform.intervention.domain.model.valueobjects.ApplicationDate;
+import com.arcadiadevs.viora.platform.intervention.domain.model.valueobjects.AppliedArea;
+import com.arcadiadevs.viora.platform.intervention.interfaces.rest.resources.CertifyApplicationResource;
+import com.arcadiadevs.viora.platform.intervention.interfaces.rest.resources.InterventionExecutionResource;
+
+public class InterventionExecutionResourceAssembler {
+
+    public static CertifyApplicationCommand toCommandFromResource(CertifyApplicationResource resource) {
+        return new CertifyApplicationCommand(
+                resource.treatmentPrescriptionId(),
+                new ApplicationDate(resource.applicationDate()),
+                new AppliedArea(resource.appliedArea()),
+                resource.executionStatus(),
+                resource.fieldNote()
+        );
+    }
+
+    public static InterventionExecutionResource toResourceFromDomain(InterventionExecution domain) {
+        return new InterventionExecutionResource(
+                domain.getId() != null ? domain.getId().value() : null,
+                domain.getTreatmentPrescriptionId().value(),
+                domain.getApplicationDate().date(),
+                domain.getAppliedArea().description(),
+                domain.getExecutionStatus(),
+                domain.getFieldNote()
+        );
+    }
+}
